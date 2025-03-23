d7139923bbe5639c6dbd0409333e085ae6bc5758
Ze Costa <zecst19@gmail.com>
2025-03-23 12:56:41 +0000

    fixed unit tests

--- server/user/user_service_test.go
+++ server/user/user_service_test.go
@@ -17,7 +17,7 @@ const (
 	dbName   = "userTestDB"
 )
 
-func TestUserService_CreateUser(t *testing.T) {
+func TestUserService(t *testing.T) {
 	ctx := context.Background()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
@@ -37,136 +37,6 @@ func TestUserService_CreateUser(t *testing.T) {
 
 	// Create a new UserService instance
 	user_collection := client.Database(dbName).Collection("users_test")
-
-	svc := NewUserService(user_collection)
-
-	// set up test cases
-	tests := map[string]struct {
-		in                *pb.CreateUserRequest
-		expected_response *pb.User
-	}{
-		"Create User Success": {
-			in: &pb.CreateUserRequest{
-				FirstName: "Cristiano",
-				LastName:  "Ronaldo",
-				Nickname:  "CR7",
-				Password:  "pass1234",
-				Email:     "cristiano@ronaldo.com",
-				Country:   "PT",
-			},
-			expected_response: &pb.User{
-				FirstName: "Cristiano",
-				LastName:  "Ronaldo",
-				Nickname:  "CR7",
-				Email:     "cristiano@ronaldo.com",
-				Country:   "PT",
-			},
-		},
-	}
-
-	for scenario, tc := range tests {
-		t.Run(scenario, func(t *testing.T) {
-			resp, err := svc.CreateUser(context.Background(), tc.in)
-			require.NoError(t, err)
-			require.Equal(t, tc.expected_response.FirstName, resp.FirstName)
-			require.Equal(t, tc.expected_response.LastName, resp.LastName)
-			require.Equal(t, tc.expected_response.Nickname, resp.Nickname)
-			require.Equal(t, tc.expected_response.Email, resp.Email)
-			require.Equal(t, tc.expected_response.Country, resp.Country)
-		})
-
-	}
-}
-
-func TestUserService_GetUser(t *testing.T) {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
-	}
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
-		}
-	}()
-
-	// Check the connection
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf("Failed to ping MongoDB: %v", err)
-	}
-
-	// Create a new UserService instance
-	user_collection := client.Database(dbName).Collection("users_test")
-
-	svc := NewUserService(user_collection)
-
-	user_collection.InsertOne(ctx, &pb.User{
-		Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
-		FirstName: "Cristiano",
-		LastName:  "Ronaldo",
-		Nickname:  "CR7",
-		Password:  "$2a$14$ol2a598AAUFAV3SizmaZJuvnBjfqA6CGzssNnCQ0Wn9Sr7vFxy5wy",
-		Email:     "cristiano@ronaldo.com",
-		Country:   "PT",
-		CreatedAt: "2025-03-22T18:37:00Z",
-		UpdatedAt: "2025-03-22T18:37:00Z",
-	},
-	)
-	// set up test cases
-	tests := map[string]struct {
-		in                *pb.GetUserRequest
-		expected_response *pb.User
-	}{
-		"Create User Success": {
-			in: &pb.GetUserRequest{
-				Id: "86f9f466-851a-4b93-af21-d5f52ac91006",
-			},
-			expected_response: &pb.User{
-				Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
-				FirstName: "Cristiano",
-				LastName:  "Ronaldo",
-				Nickname:  "CR7",
-				Password:  "$2a$14$ol2a598AAUFAV3SizmaZJuvnBjfqA6CGzssNnCQ0Wn9Sr7vFxy5wy",
-				Email:     "cristiano@ronaldo.com",
-				Country:   "PT",
-				CreatedAt: "2025-03-22T18:37:00Z",
-				UpdatedAt: "2025-03-22T18:37:00Z",
-			},
-		},
-	}
-
-	for scenario, tc := range tests {
-		t.Run(scenario, func(t *testing.T) {
-			resp, err := svc.GetUser(context.Background(), tc.in)
-			require.NoError(t, err)
-			require.Equal(t, tc.expected_response, resp)
-		})
-
-	}
-}
-
-func TestUserService_UpdateUser(t *testing.T) {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
-	}
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
-		}
-	}()
-
-	// Check the connection
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf("Failed to ping MongoDB: %v", err)
-	}
-
-	// Create a new UserService instance
-	user_collection := client.Database(dbName).Collection("users_test")
-
 	svc := NewUserService(user_collection)
 
 	user_collection.InsertOne(ctx, &pb.User{
@@ -182,139 +52,39 @@ func TestUserService_UpdateUser(t *testing.T) {
 	},
 	)
 
-	newFirstName := "Mohammed"
-	newLastName := "Salah"
-	newNickname := "MoSalah"
-	newCountry := "EG"
-
-	// set up test cases
-	tests := map[string]struct {
-		in                *pb.UpdateUserRequest
-		expected_response *pb.User
-	}{
-		"Create User Success": {
-			in: &pb.UpdateUserRequest{
-				Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
-				FirstName: &newFirstName,
-				LastName:  &newLastName,
-				Nickname:  &newNickname,
-				Country:   &newCountry,
-			},
-			expected_response: &pb.User{
-				Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
-				FirstName: "Mohammed",
-				LastName:  "Salah",
-				Nickname:  "MoSalah",
-				Email:     "cristiano@ronaldo.com",
-				Country:   "EG",
-				CreatedAt: "2025-03-22T18:37:00Z",
-				UpdatedAt: "2025-03-22T18:37:00Z",
-			},
-		},
-	}
-
-	for scenario, tc := range tests {
-		t.Run(scenario, func(t *testing.T) {
-			resp, err := svc.UpdateUser(context.Background(), tc.in)
-			require.NoError(t, err)
-			require.Equal(t, tc.expected_response.FirstName, resp.FirstName)
-			require.Equal(t, tc.expected_response.LastName, resp.LastName)
-			require.Equal(t, tc.expected_response.Nickname, resp.Nickname)
-			require.Equal(t, tc.expected_response.Email, resp.Email)
-			require.Equal(t, tc.expected_response.Country, resp.Country)
-			require.Equal(t, tc.expected_response.CreatedAt, resp.CreatedAt)
-			require.NotEqual(t, tc.expected_response.UpdatedAt, resp.UpdatedAt)
-		})
-
-	}
-}
-
-func TestUserService_DeleteUser(t *testing.T) {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
-	}
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+	t.Run("Create User", func(t *testing.T) {
+		in := &pb.CreateUserRequest{
+			FirstName: "Mohammed",
+			LastName:  "Salah",
+			Nickname:  "MoSalah",
+			Password:  "word5678",
+			Email:     "mo@salah.com",
+			Country:   "EG",
 		}
-	}()
 
-	// Check the connection
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf("Failed to ping MongoDB: %v", err)
-	}
-
-	// Create a new UserService instance
-	user_collection := client.Database(dbName).Collection("users_test")
-
-	svc := NewUserService(user_collection)
-
-	user_collection.InsertOne(ctx, &pb.User{
-		Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
-		FirstName: "Cristiano",
-		LastName:  "Ronaldo",
-		Nickname:  "CR7",
-		Password:  "$2a$14$ol2a598AAUFAV3SizmaZJuvnBjfqA6CGzssNnCQ0Wn9Sr7vFxy5wy",
-		Email:     "cristiano@ronaldo.com",
-		Country:   "PT",
-		CreatedAt: "2025-03-22T18:37:00Z",
-		UpdatedAt: "2025-03-22T18:37:00Z",
-	},
-	)
-
-	// set up test cases
-	tests := map[string]struct {
-		in                *pb.DeleteUserRequest
-		expected_response *pb.DeleteUserResponse
-	}{
-		"Create User Success": {
-			in: &pb.DeleteUserRequest{
-				Id: "86f9f466-851a-4b93-af21-d5f52ac91006",
-			},
-			expected_response: &pb.DeleteUserResponse{
-				Success: true,
-			},
-		},
-	}
-
-	for scenario, tc := range tests {
-		t.Run(scenario, func(t *testing.T) {
-			resp, err := svc.DeleteUser(context.Background(), tc.in)
-			require.NoError(t, err)
-			require.Equal(t, tc.expected_response, resp)
-		})
-
-	}
-}
-
-func TestUserService_ListUsers(t *testing.T) {
-	ctx := context.Background()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		log.Fatalf("Failed to connect to MongoDB: %v", err)
-	}
-	defer func() {
-		if err = client.Disconnect(ctx); err != nil {
-			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+		expected_response := &pb.User{
+			FirstName: "Mohammed",
+			LastName:  "Salah",
+			Nickname:  "MoSalah",
+			Email:     "mo@salah.com",
+			Country:   "EG",
 		}
-	}()
 
-	// Check the connection
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf("Failed to ping MongoDB: %v", err)
-	}
+		resp, err := svc.CreateUser(context.Background(), in)
+		require.NoError(t, err)
+		require.Equal(t, expected_response.FirstName, resp.FirstName)
+		require.Equal(t, expected_response.LastName, resp.LastName)
+		require.Equal(t, expected_response.Nickname, resp.Nickname)
+		require.Equal(t, expected_response.Email, resp.Email)
+		require.Equal(t, expected_response.Country, resp.Country)
+	})
 
-	// Create a new UserService instance
-	user_collection := client.Database(dbName).Collection("users_test")
+	t.Run("Get User", func(t *testing.T) {
+		in := &pb.GetUserRequest{
+			Id: "86f9f466-851a-4b93-af21-d5f52ac91006",
+		}
 
-	svc := NewUserService(user_collection)
-
-	user_collection.InsertOne(ctx,
-		&pb.User{
+		expected_response := &pb.User{
 			Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
 			FirstName: "Cristiano",
 			LastName:  "Ronaldo",
@@ -324,91 +94,156 @@ func TestUserService_ListUsers(t *testing.T) {
 			Country:   "PT",
 			CreatedAt: "2025-03-22T18:37:00Z",
 			UpdatedAt: "2025-03-22T18:37:00Z",
-		},
-	)
-	user_collection.InsertOne(ctx,
-		&pb.User{
-			Id:        "4bedafd6-b946-4d70-a156-d828ddcb62fb",
-			FirstName: "Mohammed",
-			LastName:  "Salah",
-			Nickname:  "MoSalah",
-			Password:  "$2a$14$ol2a598AAUFAV3SizmaZJuvnBjfqA6CGzssNnCQ0Wn9Sr7vFxy5wy",
-			Email:     "mo@salah.com",
-			Country:   "EG",
+		}
+
+		resp, err := svc.GetUser(context.Background(), in)
+		require.NoError(t, err)
+		require.Equal(t, expected_response, resp)
+
+	})
+
+	t.Run("Update User", func(t *testing.T) {
+		newFirstName := "Lionel"
+		newLastName := "Messi"
+		newNickname := "Messi"
+		newCountry := "AR"
+
+		in := &pb.UpdateUserRequest{
+			Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
+			FirstName: &newFirstName,
+			LastName:  &newLastName,
+			Nickname:  &newNickname,
+			Country:   &newCountry,
+		}
+
+		expected_response := &pb.User{
+			Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
+			FirstName: "Lionel",
+			LastName:  "Messi",
+			Nickname:  "Messi",
+			Email:     "cristiano@ronaldo.com",
+			Country:   "AR",
 			CreatedAt: "2025-03-22T18:37:00Z",
 			UpdatedAt: "2025-03-22T18:37:00Z",
-		},
-	)
-	countryFilter := "PT"
-	// set up test cases
-	tests := map[string]struct {
-		in                *pb.ListUsersRequest
-		expected_response *pb.ListUsersResponse
-	}{
-		"List User Success": {
-			in: &pb.ListUsersRequest{
-				Page:     1,
-				PageSize: 10,
-			},
-			expected_response: &pb.ListUsersResponse{
-				Users: []*pb.User{
-					&pb.User{
-						Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
-						FirstName: "Cristiano",
-						LastName:  "Ronaldo",
-						Nickname:  "CR7",
-						Password:  "$2a$14$ol2a598AAUFAV3SizmaZJuvnBjfqA6CGzssNnCQ0Wn9Sr7vFxy5wy",
-						Email:     "cristiano@ronaldo.com",
-						Country:   "PT",
-						CreatedAt: "2025-03-22T18:37:00Z",
-						UpdatedAt: "2025-03-22T18:37:00Z",
-					},
-					&pb.User{
-						Id:        "4bedafd6-b946-4d70-a156-d828ddcb62fb",
-						FirstName: "Mohammed",
-						LastName:  "Salah",
-						Nickname:  "MoSalah",
-						Password:  "$2a$14$ol2a598AAUFAV3SizmaZJuvnBjfqA6CGzssNnCQ0Wn9Sr7vFxy5wy",
-						Email:     "mo@salah.com",
-						Country:   "EG",
-						CreatedAt: "2025-03-22T18:37:00Z",
-						UpdatedAt: "2025-03-22T18:37:00Z",
-					},
-				},
-				TotalCount: 2,
-			},
-		},
-		"List User Success Filter Country": {
-			in: &pb.ListUsersRequest{
-				Page:     1,
-				PageSize: 10,
-				Country:  &countryFilter,
-			},
-			expected_response: &pb.ListUsersResponse{
-				Users: []*pb.User{
-					&pb.User{
-						Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
-						FirstName: "Cristiano",
-						LastName:  "Ronaldo",
-						Nickname:  "CR7",
-						Password:  "$2a$14$ol2a598AAUFAV3SizmaZJuvnBjfqA6CGzssNnCQ0Wn9Sr7vFxy5wy",
-						Email:     "cristiano@ronaldo.com",
-						Country:   "PT",
-						CreatedAt: "2025-03-22T18:37:00Z",
-						UpdatedAt: "2025-03-22T18:37:00Z",
-					},
-				},
-				TotalCount: 1,
-			},
-		},
-	}
+		}
 
-	for scenario, tc := range tests {
-		t.Run(scenario, func(t *testing.T) {
-			resp, err := svc.ListUsers(context.Background(), tc.in)
-			require.NoError(t, err)
-			require.Equal(t, tc.expected_response, resp)
-		})
+		resp, err := svc.UpdateUser(context.Background(), in)
+		require.NoError(t, err)
+		require.Equal(t, expected_response.FirstName, resp.FirstName)
+		require.Equal(t, expected_response.LastName, resp.LastName)
+		require.Equal(t, expected_response.Nickname, resp.Nickname)
+		require.Equal(t, expected_response.Email, resp.Email)
+		require.Equal(t, expected_response.Country, resp.Country)
+		require.Equal(t, expected_response.CreatedAt, resp.CreatedAt)
+		require.NotEqual(t, expected_response.UpdatedAt, resp.UpdatedAt)
 
-	}
+	})
+
+	t.Run("List Users", func(t *testing.T) {
+		in := &pb.ListUsersRequest{
+			Page:     1,
+			PageSize: 10,
+		}
+
+		expected_response := &pb.ListUsersResponse{
+			Users: []*pb.User{
+				{
+					Id:        "86f9f466-851a-4b93-af21-d5f52ac91006",
+					FirstName: "Lionel",
+					LastName:  "Messi",
+					Nickname:  "Messi",
+					Password:  "$2a$14$ol2a598AAUFAV3SizmaZJuvnBjfqA6CGzssNnCQ0Wn9Sr7vFxy5wy",
+					Email:     "cristiano@ronaldo.com",
+					Country:   "AR",
+					CreatedAt: "2025-03-22T18:37:00Z",
+					UpdatedAt: "2025-03-22T18:37:00Z",
+				},
+				{
+					Id:        "4bedafd6-b946-4d70-a156-d828ddcb62fb",
+					FirstName: "Mohammed",
+					LastName:  "Salah",
+					Nickname:  "MoSalah",
+					Password:  "$2a$14$SpVoNKXfhSqGP4YTkh6H6ORQCoWxN9gQt3HHreNxKl8D0Gp1bn3oa",
+					Email:     "mo@salah.com",
+					Country:   "EG",
+					CreatedAt: "2025-03-22T18:37:00Z",
+					UpdatedAt: "2025-03-22T18:37:00Z",
+				},
+			},
+			TotalCount: 2,
+		}
+
+		resp, err := svc.ListUsers(context.Background(), in)
+		require.NoError(t, err)
+		require.Equal(t, len(expected_response.Users), len(resp.Users))
+
+		require.Equal(t, expected_response.Users[0].FirstName, resp.Users[0].FirstName)
+		require.Equal(t, expected_response.Users[0].LastName, resp.Users[0].LastName)
+		require.Equal(t, expected_response.Users[0].Nickname, resp.Users[0].Nickname)
+		require.Equal(t, expected_response.Users[0].Email, resp.Users[0].Email)
+		require.Equal(t, expected_response.Users[0].Country, resp.Users[0].Country)
+
+		require.Equal(t, expected_response.Users[1].FirstName, resp.Users[1].FirstName)
+		require.Equal(t, expected_response.Users[1].LastName, resp.Users[1].LastName)
+		require.Equal(t, expected_response.Users[1].Nickname, resp.Users[1].Nickname)
+		require.Equal(t, expected_response.Users[1].Email, resp.Users[1].Email)
+		require.Equal(t, expected_response.Users[1].Country, resp.Users[1].Country)
+
+		require.Equal(t, expected_response.TotalCount, int32(2))
+	})
+
+	t.Run("List Users Country Filter", func(t *testing.T) {
+		countryFilter := "EG"
+
+		in := &pb.ListUsersRequest{
+			Page:     1,
+			PageSize: 10,
+			Country:  &countryFilter,
+		}
+
+		expected_response := &pb.ListUsersResponse{
+			Users: []*pb.User{
+				{
+					Id:        "4bedafd6-b946-4d70-a156-d828ddcb62fb",
+					FirstName: "Mohammed",
+					LastName:  "Salah",
+					Nickname:  "MoSalah",
+					Password:  "$2a$14$SpVoNKXfhSqGP4YTkh6H6ORQCoWxN9gQt3HHreNxKl8D0Gp1bn3oa",
+					Email:     "mo@salah.com",
+					Country:   "EG",
+					CreatedAt: "2025-03-22T18:37:00Z",
+					UpdatedAt: "2025-03-22T18:37:00Z",
+				},
+			},
+			TotalCount: 2,
+		}
+
+		resp, err := svc.ListUsers(context.Background(), in)
+		require.NoError(t, err)
+		require.Equal(t, len(expected_response.Users), len(resp.Users))
+
+		require.Equal(t, expected_response.Users[0].FirstName, resp.Users[0].FirstName)
+		require.Equal(t, expected_response.Users[0].LastName, resp.Users[0].LastName)
+		require.Equal(t, expected_response.Users[0].Nickname, resp.Users[0].Nickname)
+		require.Equal(t, expected_response.Users[0].Email, resp.Users[0].Email)
+		require.Equal(t, expected_response.Users[0].Country, resp.Users[0].Country)
+
+		require.Equal(t, expected_response.TotalCount, int32(2))
+	})
+
+	t.Run("Delete User", func(t *testing.T) {
+		in := &pb.DeleteUserRequest{
+			Id: "86f9f466-851a-4b93-af21-d5f52ac91006",
+		}
+
+		expected_response := &pb.DeleteUserResponse{
+			Success: true,
+		}
+
+		resp, err := svc.DeleteUser(context.Background(), in)
+		require.NoError(t, err)
+		require.Equal(t, expected_response, resp)
+	})
+
+	client.Database(dbName).Collection("users_test").Drop(ctx)
 }
